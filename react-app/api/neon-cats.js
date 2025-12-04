2f2e00eb94fd82361802cd6d6c69dbbdaebd9cb8
Varga Botond <btnd.varga@gmail.com>
2025-12-04 08:43:04 +0100

    Op2s

--- react-app/api/neon-cats.js
+++ react-app/api/neon-cats.js
@@ -1,3 +1,23 @@
+import {neon} from '@neondatabase/serverless'
 export default async function handler(req, res) {
-    return res.staus(200).json('neon-cats endpoint works B)')
+    const conn = neon(process.env.DATABASE_URL)
+    console.log('conn', conn)
+
+    if (req.method == 'GET') {
+        const result = await conn`SELECT FROM cats LIMIT 20`
+        console.log('get result', result)
+        return res.status(200).json(result)
+    }
+
+    if (req.method == 'POST') {
+
+    }
+    if (req.method == 'PATCH') {
+        
+    }
+    if (req.method == 'DELETE') {
+        
+    }
+
+    return res.staus(500).json([{error: 'Internal Server Error'}])
 }

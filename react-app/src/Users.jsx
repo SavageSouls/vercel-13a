3ee3b1c29c0b621175238d45534bbe88a697344a
Varga Botond <btnd.varga@gmail.com>
2025-12-05 10:50:18 +0100

    Firebase

--- react-app/src/Users.jsx
+++ react-app/src/Users.jsx
@@ -1,7 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { initializeApp } from "firebase/app"
 
 export default function Users(props) {
     const [users, setUsers] = useState([])
+
+    const firebaseConfig ={
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    };
+    const app = initializeApp(firebaseConfig);
+
+    useEffect(function(){
+        console.log('firebaseConfig.apiKey.length', firebaseConfig?.apiKey?.length)
+    }, [])
 
     return (
         <>
